03875b64abaeb9636038a454ef75a2d8bcd45022
Mina <mina.dursun@vektora.com>
2019-11-11 20:16:55 +0300

    service commit

--- communityProject/mysite/polls/urls.py
+++ communityProject/mysite/polls/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:id>/vote/', views.vote, name='vote'),
+    path('login', views.login),
+    # path('', views.index, name='index'),
+    # # ex: /polls/5/
+    # path('<int:id>/', views.detail, name='detail'),
+    # # ex: /polls/5/results/
+    # path('<int:id>/results/', views.results, name='results'),
+    # # ex: /polls/5/vote/
+    # path('<int:id>/vote/', views.vote, name='vote'),
 ]
--- communityProject/mysite/polls/views.py
+++ communityProject/mysite/polls/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .session_services import *
 
 # Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def detail(request, community_name):
-    return HttpResponse("You're looking at community %s." % community_name)
-
-def results(request, community_name):
-    response = "You're looking at the results of community %s."
-    return HttpResponse(response % community_name)
-
-def vote(request, community_name):
-    return HttpResponse("You're voting on community %s." % community_name)
+def login(request):
+    return HttpResponse(SessionService.login("hellow"))
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+#
+# def detail(request, community_name):
+#     return HttpResponse("You're looking at community %s." % community_name)
+#
+# def results(request, community_name):
+#     response = "You're looking at the results of community %s."
+#     return HttpResponse(response % community_name)
+#
+# def vote(request, community_name):
+#     return HttpResponse("You're voting on community %s." % community_name)
 
 
 
